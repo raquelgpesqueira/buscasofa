dbdeb8312419fcea069b1ae4843f103ecc4c473c
Reichgp <113127786+Reichgp@users.noreply.github.com>
2026-05-29 15:05:09 +0200

    Update utils.ts

--- src/apis/utils.ts
+++ src/apis/utils.ts
@@ -1,5 +1,6 @@
 
 // Función para calcular la distancia entre dos coordenadas (Haversine)
+// ****************************************************** CANDIDATA PRUEBA UNITARIA getDistanceKm -  Haversine
 function getDistanceKm(lat1: number, lon1: number, lat2: number, lon2: number) {
   const R = 6371; // Radio de la Tierra en km
   const dLat = (lat2 - lat1) * Math.PI / 180;
