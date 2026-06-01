9d79775a98939a7f952d30435d9695911a96fabc
Oscuryo <david.ramirez203@comunidadunir.net>
2026-06-01 19:47:44 +0200

    Añadida prueba Cypress para validar la pagina About

---
+++ cypress/e2e/about.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Página About', () => {
+    beforeEach(() => {
+        cy.visit('/about');
+    });
+
+    it('Muestra el número de equipo y los miembros', () => {
+
+        cy.contains('Equipo nº 23').should('exist');
+
+        cy.contains('Andy Garcia Sanchez').should('exist');
+        cy.contains('Raquel Gonzalez Pesqueira').should('exist');
+        cy.contains('David Ramirez Griño').should('exist');
+        cy.contains('Jose Ramon Fernandez Peralta').should('exist');
+        cy.contains('Adrian Miguel Almazan Tello').should('exist');
+
+    });
+});
