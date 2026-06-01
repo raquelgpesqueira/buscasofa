d51727db02dfcccca8b85b9a27493e5fc07faebf
Oscuryo <david.ramirez203@comunidadunir.net>
2026-06-01 19:31:58 +0200

    Corregidos nombres del equipo en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
     <div>
         <h2>Miembros del equipo:</h2>
         <ul>
-            <li>Javier García</li>
-            <li>Javier López</li>
-            <li>Javier Martínez</li>
-            <li>Javier Pérez</li>
-            <li>Javier Rodríguez</li>
+            <li>Andy Garcia Sanchez</li>
+            <li>Raquel Gonzalez Pesqueira</li>
+            <li>David Ramirez Griño</li>
+            <li>Jose Ramon Fernandez Peralta</li>
+            <li>Adrian Miguel Almazan Tello</li>
         </ul>
     </div>
   )
