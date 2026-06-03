d1ea754dc566aa15eb20668f81c4b096f7788fb6
Raquel G. Pesqueira <113127786+raquelgpesqueira@users.noreply.github.com>
2026-06-03 14:49:57 +0200

    Refactor Vite config to remove comments and clean up

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-/*import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -10,17 +10,4 @@ export default defineConfig({
   },
   plugins: [react()],
   // base: '/buscasofa/',
-})
-*/
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/buscasofa/',
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  plugins: [react()],
 })
